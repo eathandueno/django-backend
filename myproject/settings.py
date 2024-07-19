f534ff1f100ff057fdadd182d6f18c61b05e273f
Eathan Dueno <66228368+eathandueno@users.noreply.github.com>
2024-07-19 14:34:25 -0600

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-1y1d19&nv4&dgknm*jbw3ve5!0+ewwi&sv0w)k0t(r3deqd5^8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.149.182.89']
 
 
 # Application definition
